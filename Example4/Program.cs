943bbf98726a3519671c5683bf2f7464aa6a5c93
Aleksandr Zakharov <Resurrectist@gmail.com>
2022-11-27 20:56:39 +0300

    АФАПЫВПАЫВП

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double number1 = 14;
+double number2 = 5;
+Console.WriteLine(number1 / number2);
+
